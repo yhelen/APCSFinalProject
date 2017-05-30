a79aee9977231222aa6127bcaa5cc98fdfb96cdf
Helen Ye <00yhelen@gmail.com>
2017-05-30 13:55:54 -0400

    Add extended precision number and driver file

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static XP rand() {
+        String s = "";
+        int len = 5;//(int) (Math.random() * 99) + 1;
+        while(len > 0) {
+            s += "" + (int) (Math.random() * 10);
+            len--;
+        }
+        System.out.println("Creating XP: " + s);
+        return new XP(s);
+    }
+
+    public static void main(String[] args) {
+        XP a = rand();
+        XP b = rand();
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        XP sum = a.add(b);
+        System.out.println("sum: " + sum);
+        //System.out.println("diff: " + sum.sub(a));
+        System.out.println("a.getNumDigits: " + a.getNumDigits());
+        System.out.println("a.compareTo: " + a.compareTo(b));
+    }
+
+}
---
+++ XP.java
@@ -0,0 +1,121 @@
+public class XP implements Comparable<XP>{
+
+    private int[] num;
+    private int numDigits;
+    private static final int MAX_LENGTH = 20;
+    private static final int ALLOCATED_LEN = 2 * MAX_LENGTH + 1;
+
+    private XP(String digits, boolean initial) {
+        if(digits.length() == 0) {
+            throw new IllegalArgumentException("No number inputted");
+        }
+        
+        if(initial && digits.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("Too many digits: " + digits.length() + " > " + MAX_LENGTH);
+        }
+        
+        num = new int[ALLOCATED_LEN];
+        setNum(digits);
+        numDigits = countDigits(digits);
+    }
+
+    public XP(String digits) {
+        this(digits, true);
+    }
+
+    private void setNum(String digits) {
+        int strInd = digits.length();
+        int i = 0;
+        while(strInd > 0) {
+            num[i] = Integer.parseInt(digits.substring(strInd - 1, strInd));
+            strInd--;
+            i++;
+        }
+    }
+
+    private int countDigits(String digits) {
+        int i = ALLOCATED_LEN - 1;
+        while(i >= 0 && num[i] == 0) {
+            i--;
+        }
+        return i + 1;
+    }
+
+    public boolean isOdd() {
+        return num[0] % 2 == 1;
+    }
+
+    public XP add(XP n) {
+        if(this.getNumDigits() > MAX_LENGTH || n.getNumDigits() > MAX_LENGTH) {
+            throw new IllegalArgumentException("Numbers too big");
+        }
+        String sum = "";
+        //broken??
+        for(int i = 0; i <= Math.max(this.getNumDigits(), n.getNumDigits()); i++) {
+            int total = this.num[i] + n.num[i];
+            if(total > 9) {
+                this.num[i + 1] += total / 10;
+            }
+            sum = total % 10 + sum;
+        }
+        return new XP(sum, false);
+    }
+
+    // TODO
+    // Doesn't work --> 
+    public XP sub(XP n) {
+        //if(this.compareTo(n) < 0) {
+        //    throw new IllegalArgumentException("input must be less than this number");
+        //}
+
+        String diff = "";
+        for(int i = 0; i < ALLOCATED_LEN; i++) {
+            if(this.num[i] < n.num[i]) {
+                this.num[i + 1]--;
+                this.num[i] += 10;
+            }
+            int total = this.num[i] - n.num[i];
+            diff = total + diff;
+        }
+        return new XP(diff,false);
+    }
+
+    public XP mult(XP n) {
+        return null;
+    }
+    
+    public XP div(XP n) {
+        return null;
+    }
+
+    public int getNumDigits() {
+        return numDigits;
+    }
+
+    // TODO
+    // TEST FURTHER
+    public int compareTo(XP b) {
+        if(this.getNumDigits() != b.getNumDigits()) {
+            return this.getNumDigits() - b.getNumDigits();
+        }
+        
+        for(int i = this.getNumDigits() - 1; i >= 0; i--) {
+            if(this.num[i] != b.num[i]) {
+                return this.num[i] - b.num[i];
+            }
+        }
+        
+        return 0;
+    }
+
+    public String toString() {
+        String ans = "";
+        int i = getNumDigits() - 1;
+        while(i >= 0) {
+            ans += num[i];
+            i--;
+        }
+        return ans;
+    }
+
+}
